e9f60da954f6089c424685ee6748f62644228f9d
samyabouseda <samy@abouseda.ch>
2019-10-28 16:37:52 +0100

    Add examples endpoints and modular routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import uuidv4 from 'uuid/v4'
+import models from './models'
 
 const port = process.env.PORT
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    }
+    next()
+})
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/session', (req, res) => {
+    return res.send(req.context.models.users[req.context.me.id]);
+})
+
+app.get('/users', (req, res) => {
+    return res.send(Object.values(req.context.models.users));
+})
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(req.context.models.users[req.params.userId]);
+})
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(req.context.models.messages));
+})
+
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(req.context.models.messages[req.params.messageId]);
+})
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+        id,
+        text: req.body.text,
+        userId: req.context.me.id,
+    }
+    req.context.models.messages[id] = message
+    return res.send(message)
+})
+
+app.delete('/messages/:messageId', (req, res) => {
+    const {
+        [req.params.messageId]: message,
+        ...otherMessages
+    } = req.context.models.messages
+    req.context.models.messages = otherMessages
+    return res.send(message)
 })
 
 app.listen(port, () =>
