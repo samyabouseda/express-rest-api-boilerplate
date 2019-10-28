495cf5f749b9e5e404ba0afb68f1dcb4d05fea45
samyabouseda <samy@abouseda.ch>
2019-10-28 18:09:17 +0100

    Add prettier

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import models, { connectDb } from './models'
 import routes from './routes'
 
-const eraseDatabaseOnSync = true;
+const eraseDatabaseOnSync = true
 const port = process.env.PORT
 const app = express()
 
@@ -13,11 +13,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(async (req, res, next) => {
-    req.context = {
-        models,
-        me: await models.User.findByLogin('rwieruch'),
-    }
-    next()
+	req.context = {
+		models,
+		me: await models.User.findByLogin('rwieruch'),
+	}
+	next()
 })
 
 app.use('/session', routes.session)
@@ -25,43 +25,43 @@ app.use('/users', routes.user)
 app.use('/messages', routes.message)
 
 connectDb().then(async () => {
-    if (eraseDatabaseOnSync) {
-        await Promise.all([
-            models.User.deleteMany({}),
-            models.Message.deleteMany({}),
-        ])
-        createUsersWithMessages()
-    }
+	if (eraseDatabaseOnSync) {
+		await Promise.all([
+			models.User.deleteMany({}),
+			models.Message.deleteMany({}),
+		])
+		createUsersWithMessages()
+	}
 
-    app.listen(port, () =>
-        console.log(`Example app listening on port ${port}!`)
-    )
+	app.listen(port, () =>
+		console.log(`Example app listening on port ${port}!`),
+	)
 })
 
 const createUsersWithMessages = async () => {
-    const user1 = new models.User({
-        username: 'rwieruch',
-    })
-    const user2 = new models.User({
-        username: 'ddavids',
-    });
+	const user1 = new models.User({
+		username: 'rwieruch',
+	})
+	const user2 = new models.User({
+		username: 'ddavids',
+	})
 
-    const message1 = new models.Message({
-        text: 'Published the Road to learn React',
-        user: user1.id,
-    })
-    const message2 = new models.Message({
-        text: 'Happy to release ...',
-        user: user2.id,
-    });
-    const message3 = new models.Message({
-        text: 'Published a complete ...',
-        user: user2.id,
-    });
+	const message1 = new models.Message({
+		text: 'Published the Road to learn React',
+		user: user1.id,
+	})
+	const message2 = new models.Message({
+		text: 'Happy to release ...',
+		user: user2.id,
+	})
+	const message3 = new models.Message({
+		text: 'Published a complete ...',
+		user: user2.id,
+	})
 
-    await message1.save();
-    await message2.save();
-    await message3.save();
-    await user1.save();
-    await user2.save();
+	await message1.save()
+	await message2.save()
+	await message3.save()
+	await user1.save()
+	await user2.save()
 }
